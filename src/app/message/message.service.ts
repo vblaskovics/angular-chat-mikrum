4b99b1d1bc1adc254348f337b2ee740cdcad9d6c
Viktor Blaskovics <viktor.blaskovics@gmail.com>
2021-06-03 13:47:57 +0200

    Add messagesForThreadUser function

---
+++ src/app/message/message.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { Observable, Subject } from 'rxjs';
+import { filter } from 'rxjs/operators';
+import { Thread } from '../thread/thread.model';
+import { User } from '../user/user.model';
+import { Message } from './message.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MessageService {
+
+  newMessages: Subject<Message> = new Subject<Message>();
+
+  messages: Observable<Message[]>;
+
+  constructor() {
+
+  }
+
+  addMessage(message:Message) {
+    this.newMessages.next(message);
+  }
+
+  messagesForThreadUser(thread: Thread, user:User): Observable<Message> {
+    return this.newMessages.pipe(
+      filter((message:Message)=>{
+        return message.thread.id === thread.id && message.author.id !== user.id;
+      })
+    );
+  }
+}
