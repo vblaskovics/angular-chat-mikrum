51f737d5a5a215d1e17d2b3b9c36dc9f70c8192c
Viktor Blaskovics <viktor.blaskovics@gmail.com>
2021-06-04 13:29:06 +0200

    Add functions to chat window component

--- src/app/chat-window/chat-window.component.ts
+++ src/app/chat-window/chat-window.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Message } from '../message/message.model';
+import { MessageService } from '../message/message.service';
+import { Thread } from '../thread/thread.model';
+import { ThreadService } from '../thread/thread.service';
+import { User } from '../user/user.model';
+import { UserService } from '../user/user.service';
 
 @Component({
   selector: 'app-chat-window',
@@ -6,10 +13,44 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./chat-window.component.css']
 })
 export class ChatWindowComponent implements OnInit {
+  messages: Observable<any>;
+  currentThread: Thread;
+  currentUser: User;
+  draftMessage: Message;
 
-  constructor() { }
+  constructor(public messageService: MessageService,
+      public threadService: ThreadService,
+      public userService: UserService) { 
+        this.currentThread = new Thread();
+        this.currentUser = new User();
+        this.draftMessage = new Message();
+
+        this.messages = this.threadService.currentThreadMessages;
+
+        this.threadService.currentThread.subscribe((thread:Thread) => {
+          this.currentThread = thread;
+        });
+
+        this.userService.currentUser.subscribe((user:User) => {
+          this.currentUser = user;
+        })
+      }
 
   ngOnInit(): void {
+  }
+
+  onEnter(event: any): void {
+    this.sendMessage();
+    event.preventDefault();
+  }
+
+  sendMessage(): void {
+    const m: Message = this.draftMessage;
+    m.author = this.currentUser;
+    m.thread = this.currentThread;
+    m.isRead = true;
+    this.messageService.addMessage(m);
+    this.draftMessage = new Message();
   }
 
 }
