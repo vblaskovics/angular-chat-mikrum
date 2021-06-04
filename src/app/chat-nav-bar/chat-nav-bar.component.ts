03c98e2763211fcde115c7ef5e0833269063cd6e
Viktor Blaskovics <viktor.blaskovics@gmail.com>
2021-06-04 15:22:28 +0200

    Add navbar unread msg counter

--- src/app/chat-nav-bar/chat-nav-bar.component.ts
+++ src/app/chat-nav-bar/chat-nav-bar.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { combineLatest } from 'rxjs';
+import { Message } from '../message/message.model';
+import { MessageService } from '../message/message.service';
+import { ThreadService } from '../thread/thread.service';
 
 @Component({
   selector: 'app-chat-nav-bar',
@@ -6,10 +10,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./chat-nav-bar.component.css']
 })
 export class ChatNavBarComponent implements OnInit {
+  unreadMessagesCount?: number;
 
-  constructor() { }
+  constructor(
+    public messageService:MessageService,
+    public threadService:ThreadService) { }
 
   ngOnInit(): void {
+    combineLatest([
+      this.threadService.currentThread,
+      this.messageService.messages
+    ]).subscribe(([currentThread, messages])=>{
+      this.unreadMessagesCount = messages.reduce((sum: number, m: Message)=>{
+        const messageIsInCurrentThread: boolean = currentThread.id === m?.thread?.id;
+        if(!messageIsInCurrentThread && !m?.isRead) {
+          sum += 1;
+        }
+        return sum;
+      }, 0);
+    });
   }
 
 }
