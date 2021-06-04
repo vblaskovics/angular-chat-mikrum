2cd7919f29261620154bd81b78dec6f9d9bd9f19
Viktor Blaskovics <viktor.blaskovics@gmail.com>
2021-06-04 11:20:18 +0200

    Add (unfinished) test to thread service

--- src/app/chat-page/chat-page.component.ts
+++ src/app/chat-page/chat-page.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Message } from '../message/message.model';
 import { MessageService } from '../message/message.service';
 import { Thread } from '../thread/thread.model';
+import { ThreadService } from '../thread/thread.service';
 import { User } from '../user/user.model';
 import { UserService } from '../user/user.service';
 
@@ -12,14 +13,18 @@ import { UserService } from '../user/user.service';
 })
 export class ChatPageComponent implements OnInit {
   messages: Message[];
+  orderedThreads: Thread[];
 
   constructor(
     private userService: UserService,
-    private messageService: MessageService
+    private messageService: MessageService,
+    private threadService: ThreadService
   ) {
   }
 
   ngOnInit(): void {
+    
+
   }
 
 }
--- src/app/thread/thread.service.spec.ts
+++ src/app/thread/thread.service.spec.ts
@@ -3,14 +3,54 @@ import { TestBed } from '@angular/core/testing';
 import { ThreadService } from './thread.service';
 
 describe('ThreadService', () => {
-  let service: ThreadService;
+  let threadService: ThreadService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
-    service = TestBed.inject(ThreadService);
+    threadService = TestBed.inject(ThreadService);
   });
 
   it('should be created', () => {
-    expect(service).toBeTruthy();
+    // TODO convert to unit test (add expects...)
+
+    // const nate: User = new User("Nate Murray", "");
+    // const felipe: User = new User("Felipe Coury", "");
+
+    // const t1: Thread = new Thread("t1", "Thread 1", "");
+    // const t2: Thread = new Thread("t2", "Thread 2", "");
+
+    // const m1: Message = new Message({
+    //   author: nate,
+    //   text: "Hi!",
+    //   thread: t1,
+    // });
+
+    // const m2: Message = new Message({
+    //   author: felipe,
+    //   text: "Where did you get that hat?",
+    //   thread: t1,
+    // });
+
+    // this.threadService.threads.subscribe((thrdIdx: {[k:string]:Thread})=>{
+    //   const threads: Thread[] = Object.values(thrdIdx);
+    //   const threadNames: string = threads.map((t:Thread) => t.name).join(", ");
+    //   console.log(`=> threads length:${threads.length} names: ${threadNames}`)
+    // })
+
+    // this.threadService.orderedThreads.subscribe((threads: Thread[]) => {
+    //   this.orderedThreads = threads;
+    // })
+
+    // this.messageService.addMessage(m1);
+    // this.messageService.addMessage(m2);
+
+    // setTimeout(() => {
+    //   const m3: Message = new Message({
+    //     author: nate,
+    //     text: "This is the 3rd",
+    //     thread: t2
+    //   })
+    //   this.messageService.addMessage(m3);
+    // }, 2000)
   });
 });
