8b968cb51774fc58f1d783c99528bdb8736dbbeb
Viktor Blaskovics <viktor.blaskovics@gmail.com>
2021-06-04 13:14:15 +0200

    Add threads component to chat-page

--- src/app/chat-thread/chat-thread.component.ts
+++ src/app/chat-thread/chat-thread.component.ts
@@ -5,21 +5,22 @@ import { ThreadService } from '../thread/thread.service';
 @Component({
   selector: 'app-chat-thread',
   templateUrl: './chat-thread.component.html',
-  styleUrls: ['./chat-thread.component.css']
+  styleUrls: ['./chat-thread.component.css'],
 })
 export class ChatThreadComponent implements OnInit {
   @Input() thread: Thread;
   selected: boolean = false;
 
-  constructor(public threadService: ThreadService) { }
+  constructor(public threadService: ThreadService) {}
 
   ngOnInit(): void {
-    
+    this.threadService.currentThread.subscribe((currentThread: Thread) => {
+      this.selected = currentThread?.id === this.thread?.id;
+    });
   }
 
-  clicked(event:any): void {
+  clicked(event: any): void {
     this.threadService.setCurrentThread(this.thread);
     event.preventDefault();
   }
-
 }
